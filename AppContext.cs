6e0b96ec8c281ff1f427f3d9d09757f317651715
Tiz <dannilo@bk.ru>
2025-02-12 12:32:44 +0400

    Task 25.2.4 Done

--- AppContext.cs
+++ AppContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace PeopleAndBook
 {
-    internal class AppContext
+    public class AppContext : DbContext
     {
+        // Объекты таблицы Users
+        public DbSet<User> Users { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"DServer=localhost\SQLEXPRESS;Database=EF;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace PeopleAndBook
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PeopleAndBook
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using (var db = new PeopleAndBook.AppContext())
+            {
+                var user1 = new User { Name = "Arthur", Email = "Admin" };
+                var user2 = new User { Name = "Klim", Email = "User" };
+
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+                Console.ReadKey();
+            }
         }
     }
 }
