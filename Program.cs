e017012a06bb629bc682167ece8aeba36243163b
Tiz <dannilo@bk.ru>
2025-02-12 12:25:38 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PeopleAndBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
